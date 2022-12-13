a68feb64d855812767740d71cb86fcfcafe1b75a
Zinaida Tian <destinex@yandex.ru>
2022-12-13 18:06:09 +0400

    One For Hundred

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        int accountMoney = 100; // деньги на счету клиента
+        int amountToReplenish = 1200; // сумма пополнени€ счета
+        int totalAmount = amountToReplenish + accountMoney; // итоговый счет
+        int bonus = (totalAmount/100);
+        System.out.println(bonus);
+
+
+        // ќбъ€вл€ете переменные дл€ входных данных и
+        // параметров программы: начального счЄта,
+        // суммы пополнени€ и тп
+
+        // ”словным оператором провер€ете, превысила ли
+        // сумма пополнени€ порог, и дл€ этих двух разных
+        // сценариев рассчитываете сумму бонуса и выводите
+        // на экран.
+    }
+}
